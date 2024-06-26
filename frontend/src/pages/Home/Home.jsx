42e5f8206ecac29f5e80c7e63ca1e203b621d3a6
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-06-26 12:38:52 +0530

    ..

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react"
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
 import Header from "../../components/Header/Header"
 import "./Home.css"
 
 const Home = () => {
+    const [category, setCategory]=useState("ALL");
   return (
    <div className="home">
     <Header/>
-    <ExploreMenu/>
+    <ExploreMenu category={category} setCategory={setCategory}/>
    </div>
   )
 }
