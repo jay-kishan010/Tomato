e75043e5025d7b0e77dff8cc1b96c2ce580f7141
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-07-07 21:52:48 +0530

    ..

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,14 @@
 
 import "./AppDownload.css"
-
+import {assets} from "../../assets/assets"
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+<div className="app-download" id="app-download">
+    <p>For Better Experience Download <br />Tomato App</p>
+    <div className="app-download-platforms">
+        <img src={assets.play_store} alt="" />
+    </div>
+</div>
   )
 }
 
