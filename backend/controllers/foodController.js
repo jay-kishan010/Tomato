5d6b199f236fc680a2829f384a6c5d8564365659
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-07-09 21:03:01 +0530

    ..

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -6,6 +6,25 @@ import fs from 'fs';
 
 
 const addFood=async(req,res)=>{
+    
+     let image_filename=`${req.file.filename}`
+     const food=new foodModel({
+        name:req.body.name,
+        price:req.body.price,
+        image:image_filename,
+        description:req.body.description,
+        category:req.body.category,
+        
+
+     })
+    try {
+        await food.save();
+        res.json({sucess:true, message:"Food Added"})
+  
+    } catch (error) {
+  console.log(error) 
+  res.json({message:false, message:"Error"})     
+    }
 
 }
 
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -6,8 +6,18 @@ import multer from "multer";
 
 const foodRouter =express.Router();
 
+// image storage engine 
+const storage=multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+    return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
 
-foodRouter.post("/add",addFood)
+const upload=multer({storage:storage})
+
+
+foodRouter.post("/add",upload.single("image"),addFood)
 
 
 export default foodRouter;
