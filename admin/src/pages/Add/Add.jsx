81e1b46a509fb152c59529709cde032016ae3406
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-07-10 22:14:16 +0530

    ..

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,9 +1,55 @@
-import "./Add.css"
+import { assets } from "../../assets/assets";
+import "./Add.css";
 
 const Add = () => {
   return (
-    <div>Add</div>
-  )
-}
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" name="" hidden required id="image" />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product Name</p>
+          <input type="text" name="name" placeholder="Type here" />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product Description</p>
+          <textarea
+            name="description"
+            rows="6"
+            placeholder="Write Content here"
+            id=""
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product Category</p>
+            <select name="category" id="">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Deserts">Deserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product Price</p>
+            <input type="number" name="price" id="" placeholder="$20" />
+          </div>
+        </div>
+        <button type="submit" className="add-button">
+          Add{" "}
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Add
+export default Add;
