a4a80ea8dde6d3742403646f29cb5c184d898a9e
jay-kishan010 <124438245+jay-kishan010@users.noreply.github.com>
2024-07-10 10:29:19 +0530

    ..

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,23 @@
-
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+import {Routes, Route} from "react-router-dom"
+import Add from "./pages/Add/Add"
+import List from "./pages/List/List"
+import Order from "./pages/Order/Order"
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+<Route path="/add" element={<Add/>}/>
+<Route path="/list" element={<List/>}/>
+<Route path="/order" element={<Order/>}/>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
