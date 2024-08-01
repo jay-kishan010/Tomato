73843515f1821458ca4ea24947967513f113045c
Jay Kishan <124438245+jay-kishan010@users.noreply.github.com>
2024-08-01 17:52:02 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Order from "./pages/Order/Order"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url="https://tomato-backend-zel0.onrender.com"
+  const url="https://tomato-backend-layw.onrender.com"
   return (
     <div>
     <ToastContainer/>
